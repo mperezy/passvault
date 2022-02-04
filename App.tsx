26f9aeaa423f03e2cfcd8d55272c5c19e90f7ab0
Manuel Perez <mperezy@unitru.edu.pe>
2022-02-04 00:00:34 -0500

    Add a new constraint in the validation if the onBoarding screen was viewed
    - Add a new comparison in order to check if the onBoardingViewed flag exists in order to render the onBoarding screen or not

--- App.tsx
+++ App.tsx
@@ -37,7 +37,7 @@ const App = () => {
   useEffect(() => {
     getOnBoardingViewed()
       .then((value) => {
-        if (value === 'false') {
+        if (value === 'false' || value === undefined) {
           setOnBoardingViewed(false);
         } else {
           setOnBoardingViewed(true);
